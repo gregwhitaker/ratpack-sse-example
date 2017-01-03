7ab20012d830e2fb053479809010d4fef89114f7
Greg Whitaker <greg@gradle.com>
2017-01-02 16:33:48 -0800

    Added license

--- src/main/java/com/github/gregwhitaker/ratpacksse/example/Main.java
+++ src/main/java/com/github/gregwhitaker/ratpacksse/example/Main.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 Greg Whitaker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.gregwhitaker.ratpacksse.example;
 
 import ratpack.guice.Guice;
@@ -5,6 +21,9 @@ import ratpack.rx.RxRatpack;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 
+/**
+ * Example application that returns system metrics to clients via Server-Sent Events.
+ */
 public class Main {
 
     /**
